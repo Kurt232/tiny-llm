b8e552e691b6fae265dcc7b0008bc0f3afdc512c
DU Wenjie <duwenjie24@gmail.com>
2025-06-13 01:11:36 +0800

    Task 3: Qwen2 Grouped Query Attention

--- src/tiny_llm/qwen2_week1.py
+++ src/tiny_llm/qwen2_week1.py
@@ -24,7 +24,26 @@ class Qwen2MultiHeadAttention:
         max_seq_len: int = 32768,
         theta: int = 1000000,
     ):
-        pass
+        self.hidden_size = hidden_size
+        self.num_heads = num_heads
+        self.num_kv_heads = num_kv_heads
+        self.head_dim = hidden_size // num_heads
+        assert hidden_size % num_heads == 0
+        self.scale = mx.rsqrt(self.head_dim)
+        assert wq.shape == (hidden_size, num_heads * self.head_dim)
+        assert wk.shape == (num_kv_heads * self.head_dim, hidden_size)
+        assert wv.shape == (num_kv_heads * self.head_dim, hidden_size)
+        assert wo.shape == (num_heads * self.head_dim, hidden_size)
+
+        self.wq = wq
+        self.wk = wk
+        self.wv = wv
+        self.wo = wo
+        self.bq = bq
+        self.bk = bk
+        self.bv = bv
+
+        self.rope = RoPE(dims=self.head_dim, seq_len=max_seq_len, base=theta, traditional=False)
 
     def __call__(
         self,
@@ -32,7 +51,44 @@ class Qwen2MultiHeadAttention:
         offset: int,
         mask: mx.array | str | None = None,
     ) -> mx.array:
-        pass
+        """
+        ```
+        x: B, L, E
+        q = linear(x, wq, bq) -> B, L, H_q, D
+        k = linear(x, wk, bk) -> B, L, H, D
+        v = linear(x, wv, bv) -> B, L, H, D
+        q = rope(q, offset=slice(offset, offset + L))
+        k = rope(k, offset=slice(offset, offset + L))
+        (transpose as needed)
+        x = scaled_dot_product_attention_grouped(q, k, v, scale, mask) -> B, L, H_q, D ; Do this at float32 precision
+        (transpose as needed)
+        x = linear(x, wo) -> B, L, E
+        ```
+        """
+        B, L, _ = x.shape
+        q = linear(x, self.wq, self.bq).reshape(
+            B, L, self.num_heads, self.head_dim
+        )
+        k = linear(x, self.wk, self.bk).reshape(
+            B, L, self.num_kv_heads, self.head_dim
+        )
+        v = linear(x, self.wv, self.bv).reshape(
+            B, L, self.num_kv_heads, self.head_dim
+        )
+        q = self.rope(q, offset=slice(offset, offset + L))
+        k = self.rope(k, offset=slice(offset, offset + L))
+        q = q.transpose(0, 2, 1, 3)
+        k = k.transpose(0, 2, 1, 3)
+        v = v.transpose(0, 2, 1, 3)
+        x = scaled_dot_product_attention_grouped(
+            q,
+            k,
+            v,
+            scale=self.scale,
+            mask=mask,
+        )
+        x = x.transpose(0, 2, 1, 3).reshape(B, L, self.hidden_size)
+        return linear(x, self.wo)
 
 
 class Qwen2MLP:
