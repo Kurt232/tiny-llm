dc1a891d552a9f4d6b26b747de5fe320417bcfc3
DU Wenjie <duwenjie24@gmail.com>
2025-06-13 01:13:30 +0800

    Week 1 Day 4: RMSNorm and Multi Perceptron Layer
    Task 1: Implement `RMSNorm`
    
    Task 2: Implement the MLP Block

--- src/tiny_llm/layer_norm.py
+++ src/tiny_llm/layer_norm.py
@@ -3,7 +3,13 @@ import mlx.core as mx
 
 class RMSNorm:
     def __init__(self, dim: int, weight: mx.array, eps: float = 1e-5):
-        pass
+        self.dim = dim
+        self.weight = weight.astype(mx.float32)
+        self.eps = eps
 
     def __call__(self, x: mx.array) -> mx.array:
-        pass
+        assert x.shape[-1] == self.dim
+        dtype = x.dtype
+        x = x.astype(mx.float32)
+        x = x * mx.rsqrt(mx.mean(x * x, axis=-1, keepdims=True) + self.eps) * self.weight
+        return x.astype(dtype)
