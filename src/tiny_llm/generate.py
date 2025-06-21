cbf930045850c471c3ae18515ee7dc7f7644518d
DU Wenjie <duwenjie24@gmail.com>
2025-06-22 01:48:40 +0800

    Week 1 Day 6 Simple generate

--- src/tiny_llm/generate.py
+++ src/tiny_llm/generate.py
@@ -11,7 +11,28 @@ def simple_generate(
     prompt: str,
     sampler: Callable[[mx.array], mx.array] | None,
 ) -> str:
-    pass
+    def _step(model: Qwen2ModelWeek1, y: mx.array, offset: int) -> mx.array:
+        logits = model(y[None], offset)
+        logits = logits[:, -1, :]
+        logprobs = logits - mx.logsumexp(logits, keepdims=True)
+        if sampler is not None:
+            next = sampler(logprobs)
+        else:
+            next = mx.argmax(logprobs, axis=-1)  # N.. x 1
+        return next
+
+    inputs = mx.array(tokenizer.encode(prompt, add_special_tokens=False))
+    detokenizer = tokenizer.detokenizer
+    detokenizer.reset()
+    # generate/decode
+    while True:
+        next = _step(model, inputs, inputs.size)
+        mx.eval(next)
+        inputs = mx.concat([inputs, next])
+        if next.item() == tokenizer.eos_token_id:
+            break
+        detokenizer.add_token(next.item())
+        print(detokenizer.last_segment, end="", flush=True)
 
 
 def simple_generate_with_kv_cache(
